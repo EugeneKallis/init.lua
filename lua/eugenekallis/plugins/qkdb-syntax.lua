91118e4e724351362a9057fff67fcdb2631793ad
Eugene Kallivrousis <ekallivrousis@gmail.com>
2026-07-15 13:30:31 -0400

    fix: add explicit filetype detection for .q/.k files

--- lua/eugenekallis/plugins/qkdb-syntax.lua
+++ lua/eugenekallis/plugins/qkdb-syntax.lua
@@ -1,4 +1,12 @@
 return {
   'katusk/vim-qkdb-syntax',
   lazy = false,
+  init = function()
+    vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
+      pattern = { '*.q', '*.k' },
+      callback = function(args)
+        vim.bo[args.buf].filetype = 'q'
+      end,
+    })
+  end,
 }
